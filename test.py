6d8f8f95cd6dfe57b028c9fec19f088a6412dcae
Animesh Mod <modanimesh@gmail.com>
2018-12-04 21:18:14 -0500

    Removed incomplete script

--- test.py
+++
@@ -1,8 +0,0 @@
-strs = ["flower","flow","flight"]
-shortest = min(strs,key=len)
-#for i, ch in enumerate(shortest):
-#   print (i,ch)
-for i in range(len(strs[1])):
-    for other in strs:
-        print (other[i])
-        
